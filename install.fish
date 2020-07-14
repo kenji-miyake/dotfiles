282a95b0c205db6ca2f5ae635a8d5b4e52f2294e
Kenji Miyake <kenji.miyake@tier4.jp>
2020-07-14 23:46:20 +0900

    Fix install.fish
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- install.fish
+++ install.fish
@@ -5,7 +5,7 @@ set -l script_dir (realpath (dirname (status --current-filename)))
 set -q XDG_CONFIG_HOME || set -l XDG_CONFIG_HOME $HOME/.config
 
 for dir_name in "HOME" "XDG_CONFIG_HOME"
-    set -l dotfiles (cd $dir_name && fd -H -t f . && cd $script_dir)
+    set -l dotfiles (builtin cd $dir_name && fd -H -t f . && builtin cd $script_dir)
     for f in $dotfiles
         mkdir -pv (dirname $f)
         ln -snfv $script_dir/$dir_name/$f $$dir_name/$f
