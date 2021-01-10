da0e66bf26672aacece8563570d36c27027fc5df
Kenji Miyake <kenji.miyake@tier4.jp>
2021-01-10 10:02:02 +0900

    Overwrite SHELL variable
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- XDG_CONFIG_HOME/fish/config.fish
+++ XDG_CONFIG_HOME/fish/config.fish
@@ -1,3 +1,6 @@
+# Set $SHELL for venv in VSCode
+set SHELL (which fish)
+
 # ROS
 set -q ROS_DISTRO && source /opt/ros/$ROS_DISTRO/share/rosbash/rosfish
 alias colcon='__colcon_find_workspace_dir > /dev/null && cd (__colcon_find_workspace_dir); command colcon'
