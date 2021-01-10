82a1901fe388d16a794a4f923cefee8e42756bf2
Kenji Miyake <kenji.miyake@tier4.jp>
2021-01-10 09:41:46 +0900

    Fix bug of install.fish
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- install.fish
+++ install.fish
@@ -7,7 +7,7 @@ set -q XDG_CONFIG_HOME || set -l XDG_CONFIG_HOME $HOME/.config
 for dir_name in "HOME" "XDG_CONFIG_HOME"
     set -l dotfiles (builtin cd $dir_name && fd -H -t f . && builtin cd $script_dir)
     for f in $dotfiles
-        mkdir -pv (dirname $f)
+        mkdir -pv $$dir_name/(dirname $f)
         ln -snfv $script_dir/$dir_name/$f $$dir_name/$f
     end
 end
