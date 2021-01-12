4c7d2f18c7e053d4fd106a1ec6e772f41243e4a6
Kenji Miyake <kenji.miyake@tier4.jp>
2021-01-12 20:52:20 +0900

    Support ROS2 completion
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- XDG_CONFIG_HOME/fish/config.fish
+++ XDG_CONFIG_HOME/fish/config.fish
@@ -4,6 +4,14 @@ set SHELL (which fish)
 # pipx
 register-python-argcomplete --shell fish pipx | source
 
-# ROS
-set -q ROS_DISTRO && source /opt/ros/$ROS_DISTRO/share/rosbash/rosfish
+# colcon
 alias colcon='__colcon_find_workspace_dir > /dev/null && cd (__colcon_find_workspace_dir); command colcon'
+
+# ROS
+if set -q ROS_DISTRO
+  if [ "$ROS_VERSION" = "1" ]
+    source /opt/ros/noetic/share/rosbash/rosfish
+  else
+    register-python-argcomplete --shell fish ros2 | source
+  end
+end
