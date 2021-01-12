27a782bc7f132053eb8c281559926c93ca154fe4
Kenji Miyake <kenji.miyake@tier4.jp>
2021-01-12 20:49:56 +0900

    Add pipx completion
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- XDG_CONFIG_HOME/fish/config.fish
+++ XDG_CONFIG_HOME/fish/config.fish
@@ -1,6 +1,9 @@
 # Set $SHELL for venv in VSCode
 set SHELL (which fish)
 
+# pipx
+register-python-argcomplete --shell fish pipx | source
+
 # ROS
 set -q ROS_DISTRO && source /opt/ros/$ROS_DISTRO/share/rosbash/rosfish
 alias colcon='__colcon_find_workspace_dir > /dev/null && cd (__colcon_find_workspace_dir); command colcon'
